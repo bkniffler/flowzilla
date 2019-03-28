bfd192822d6d0f2b1c8806be8f67e8dff878d8a1
Benjamin Kniffler <bkniffler@me.com>
2019-03-28 09:19:46 +0100

    browser build

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import autoExternal from 'rollup-plugin-auto-external';
+import { resolve as r } from 'path';
+import typescript from 'rollup-plugin-typescript';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'serviceDog',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      typescript({}),
+      resolve(), // so Rollup can find `ms`
+      commonjs() // so Rollup can convert `ms` to an ES module
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: pkg.main, format: 'cjs' }],
+    plugins: [typescript({}), autoExternal()]
+  }
+];
