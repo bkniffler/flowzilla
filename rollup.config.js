506898b819907452dc009baf1a07450515aa6d75
Benjamin Kniffler <bkniffler@me.com>
2019-03-28 10:19:26 +0100

    cleanup, tslib

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,24 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import autoExternal from 'rollup-plugin-auto-external';
-import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 export default [
   {
-    input: 'src/index.ts',
+    input: pkg.module,
     output: {
       name: 'serviceDog',
       file: pkg.browser,
       format: 'umd'
     },
     plugins: [
-      typescript({}),
       resolve(), // so Rollup can find `ms`
       commonjs() // so Rollup can convert `ms` to an ES module
     ]
   },
   {
-    input: 'src/index.ts',
+    input: pkg.module,
     output: [{ file: pkg.main, format: 'cjs' }],
-    plugins: [typescript({}), autoExternal()]
+    plugins: [autoExternal()]
   }
 ];
